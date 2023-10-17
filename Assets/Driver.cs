92dd41251abb45836710bc39d2645cc929408f95
relay-mccoon <rileymccune@gmail.com>
2023-10-17 10:26:19 -0500

    updating driver

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 0.05f;
+    [SerializeField] float moveSpeed = 0.03f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,9 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float speed = Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveSpeed * speed, 0);
     }
 }
